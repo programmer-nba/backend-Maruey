bab7be73a3a6280add7c54ffda1673205ecd8e24
KieKoongDev <kiekoong.dev@gmail.com>
2024-07-30 13:26:12 +0700

    ds

--- controllers/order/orderMLM_controller.js
+++ controllers/order/orderMLM_controller.js
@@ -67,4 +67,19 @@ exports.getShippingTypes = (req, res) => {
     });
 };
 
+exports.getOrderStatuses = (req, res) => {
+    connection.query('SELECT * FROM dataset_order_status', (err, results, fields) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.status(200).json({
+            message: 'success',
+            status: true,
+            data: results
+        });
+    });
+};
+
 

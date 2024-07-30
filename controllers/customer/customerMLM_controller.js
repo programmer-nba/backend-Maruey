b3f261e865f5bc1746be77a27c116952af0afa65
KieKoongDev <kiekoong.dev@gmail.com>
2024-07-30 16:15:29 +0700

    ds

--- controllers/customer/customerMLM_controller.js
+++ controllers/customer/customerMLM_controller.js
@@ -44,3 +44,25 @@ exports.getUserEwalletTransfer = (req, res) => {
     });
 };
 
+exports.checkIntroduceUser = async (req, res) => {
+    const customerId = req.params.id;
+    if (!customerId) {
+        return res.status(400).json({
+          message: 'Customer ID is required',
+          status: false
+        });
+    }
+    connection.query('SELECT * FROM customers WHERE user_name = ?', [customerId], (err, results, fields) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.status(200).json({
+            message: 'success',
+            status: true,
+            data: results
+        });
+    });
+}
+
--- routes/customer/customerMLM.js
+++ routes/customer/customerMLM.js
@@ -4,5 +4,6 @@ const Customer = require("../../controllers/customer/customerMLM_controller")
 
 router.get('/:id/address', Customer.getUserAddress);
 router.get('/:id/ewallet', Customer.getUserEwalletTransfer);
+router.get('/check-user/:id', Customer.checkIntroduceUser);
 
 module.exports = router;

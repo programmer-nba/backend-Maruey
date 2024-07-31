d9a6e4ddae511955309ed6410f1610b60d7b24a3
KieKoongDev <kiekoong.dev@gmail.com>
2024-07-31 11:13:29 +0700

    ds

--- controllers/customer/customerMLM_controller.js
+++ controllers/customer/customerMLM_controller.js
@@ -66,3 +66,108 @@ exports.checkIntroduceUser = async (req, res) => {
     });
 }
 
+exports.getUserBank = async (req, res) => {
+    const customerId = req.params.id;
+    if (!customerId) {
+        return res.status(400).json({
+          message: 'Customer ID is required',
+          status: false
+        });
+    }
+    connection.query('SELECT * FROM customers_bank WHERE customers_id = ?', [customerId], (err, results, fields) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.status(200).json({
+            message: 'success',
+            status: true,
+            data: results
+        });
+    });
+}
+
+exports.getUserAddressDelivery = async (req, res) => {
+    const customerId = req.params.id;
+    if (!customerId) {
+        return res.status(400).json({
+          message: 'Customer ID is required',
+          status: false
+        });
+    }
+    connection.query('SELECT * FROM customers_address_delivery WHERE customers_id = ?', [customerId], (err, results, fields) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.status(200).json({
+            message: 'success',
+            status: true,
+            data: results
+        });
+    });
+}
+
+exports.upsertUserAddressDelivery = async (req, res) => {
+    const { 
+        customers_id, 
+        address, 
+        moo,
+        soi,
+        road,
+        tambon,
+        province,
+        zipcode,
+        phone,
+        status // 1, 2
+    } = req.body;
+
+    if (!customers_id || !status) {
+        return res.status(400).json({
+            message: 'customers_id and status fields are required',
+            status: false
+        });
+    }
+
+    const query = `
+        INSERT INTO customers_address_delivery (customers_id, address, moo, soi, road, tambon, province, zipcode, phone, status)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ON DUPLICATE KEY UPDATE
+            address = VALUES(address), 
+            moo = VALUES(moo),
+            soi = VALUES(soi),
+            road = VALUES(road),
+            tambon = VALUES(tambon),
+            province = VALUES(province),
+            zipcode = VALUES(zipcode),
+            phone = VALUES(phone),
+            status = VALUES(status)
+    `;
+
+    connection.query(query, [
+        customers_id, 
+        address, 
+        moo,
+        soi,
+        road,
+        tambon,
+        province,
+        zipcode,
+        phone,
+        status
+    ], (err, results, fields) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.status(200).json({
+            message: 'success',
+            status: true,
+            data: results
+        });
+    });
+}
+
--- routes/customer/customerMLM.js
+++ routes/customer/customerMLM.js
@@ -5,5 +5,7 @@ const Customer = require("../../controllers/customer/customerMLM_controller")
 router.get('/:id/address', Customer.getUserAddress);
 router.get('/:id/ewallet', Customer.getUserEwalletTransfer);
 router.get('/check-user/:id', Customer.checkIntroduceUser);
+router.get('/:id/delivery', Customer.getUserAddressDelivery);
+router.post('/delivery', Customer.upsertUserAddressDelivery);
 
 module.exports = router;

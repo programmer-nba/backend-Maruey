84de45930431f9a994c7070a4fd71c13d4063f91
KieKoongDev <kiekoong.dev@gmail.com>
2024-08-01 10:45:14 +0700

    ew

--- mysql_db.js
+++ mysql_db.js
@@ -1,38 +1,22 @@
 const mysql = require('mysql2');
 
-let connection;
+const pool = mysql.createPool({
+  host: '203.146.170.155',
+  user: 'zgolf4_mlm',
+  password: 'mWxHMaxJM6xYJjVEN8hy',
+  database: 'zgolf4_mlm',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-const handleDisconnect = () => {
-  connection = mysql.createConnection({
-    host: '203.146.170.155',
-    user: 'zgolf4_mlm',
-    password: 'mWxHMaxJM6xYJjVEN8hy',
-    database: 'zgolf4_mlm'
-  });
-  
-  connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting to the database:', err);
-      setTimeout(handleDisconnect, 2000); // Retry connection after 2 seconds
-    } else {
-      console.log('Connected to the MySQL database.');
-    }
-  });
+pool.on('connection', (connection) => {
+  console.log('New connection established:', connection.threadId);
+});
 
-  connection.on('error', (err) => {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection was closed.');
-      handleDisconnect();
-    } else if (err.code === 'ECONNRESET') {
-      console.error('Connection was reset.');
-      handleDisconnect();
-    } else {
-      console.error('Database error:', err);
-      throw err;
-    }
-  });
-};
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client:', err);
+  process.exit(-1);
+});
 
-handleDisconnect();
-
-module.exports = connection;
+module.exports = pool.promise(); // Use promise-based pool

a4ff12e358c18efd2a6404f5dddc1827f8b5ea95
KieKoong <kiekoong.dev@gmail.com>
2024-08-16 10:53:15 +0700

    gfgf

--- routes/customer/pv.js
+++ routes/customer/pv.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const Pv = require('../../controllers/customer/pvController');
 
 router.post('/jangpv/active', Pv.jangPvActive);
-router.post('/jangpv/upgrate', Pv.jangPvUpgrade);
+router.post('/jangpv/upgrade', Pv.jangPvUpgrade);
 
 module.exports = router
